7d4940c19a296509d8b15609d43d9a9ce3cacdc5
oluwatosin-me <henryogunrinde@yahoo.com>
2021-10-24 06:05:25 +0100

    enable paypal by default

--- src/Multipay/config/multipay.php
+++ src/Multipay/config/multipay.php
@@ -26,7 +26,7 @@ return [
         'paypal' => [
             'callbackRoute' => 'gateway-callback.paypal',
             'driver'        => \NeoScrypts\Multipay\Drivers\PaypalDriver::class,
-            'enable'        => (bool)env('PAYPAL_ENABLE', false),
+            'enable'        => (bool)env('PAYPAL_ENABLE', true),
             'client_env'    => env('PAYPAL_CLIENT_ENV', 'live'),
             'client_id'     => env('PAYPAL_CLIENT_ID'),
             'client_secret' => env('PAYPAL_CLIENT_SECRET')
