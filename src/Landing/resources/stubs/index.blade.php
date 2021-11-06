af20c86bff711f9bebb968c3a3c4a48c05eb9d49
oluwatosin-me <henryogunrinde@yahoo.com>
2021-11-06 17:04:30 +0100

    added landing page

---
+++ src/Landing/resources/stubs/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" data-kit-theme="default">
+
+<head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+
+    <title>{{$data['name']}}</title>
+
+    <script type="text/javascript">
+        window.__APP__ = @json($data);
+    </script>
+</head>
+
+<body>
+
+<div id="root">
+    <noscript>
+        <div style="margin-left: 20px;">
+            You need to enable JavaScript to run this app.
+        </div>
+    </noscript>
+</div>
+
+<!-- Scripts Goes Here -->
+
+</body>
+
+</html>
