81323dfb330b5b715a1a7288fcf0ab2fad52133c
oluwatosin-me <henryogunrinde@yahoo.com>
2021-11-25 11:25:37 +0100

    exclude landing page for authenticated users

--- src/Landing/LandingServiceProvider.php
+++ src/Landing/LandingServiceProvider.php
@@ -49,7 +49,7 @@ class LandingServiceProvider extends ServiceProvider
     protected function registerRoutes()
     {
         if (View::exists('landing::index')) {
-            Route::middleware('web')->group(function () {
+            Route::middleware(['web', 'guest'])->group(function () {
                 Route::get('/landing/{any?}', [IndexController::class, 'view'])->where('any', '.*');
                 Route::get('/', [IndexController::class, 'view'])->name('landing');
             });
