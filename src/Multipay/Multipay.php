eeccc41118cabb5848e06bda5d029c509b902b17
oluwatosin-me <henryogunrinde@yahoo.com>
2022-03-19 14:17:01 +0100

    use driver interface directly

--- src/Multipay/Multipay.php
+++ src/Multipay/Multipay.php
@@ -14,70 +14,25 @@ class Multipay
     protected array $config = [];
 
     /**
-     * Multipay gateway instance.
-     *
-     * @var Contracts\DriverInterface
-     */
-    protected $gateway;
-
-    /**
      * Create a new instance.
      *
      * @param array $config
-     * @param string|null $gateway
      */
-    public function __construct(array $config, string $gateway = null)
+    public function __construct(array $config)
     {
-        $this->config = tap($config, function ($config) use ($gateway) {
-            $gateway = $gateway ?: Arr::get($config, 'default');
-            $driverConfig = Arr::get($config, 'gateways.' . $gateway, []);
-            $driver = Arr::pull($driverConfig, 'driver');
-            $this->gateway = new $driver($driverConfig);
-        });
+        $this->config = $config;
     }
 
     /**
-     * Set gateway
+     * Get driver
      *
      * @param $gateway
-     * @return $this
+     * @return Contracts\DriverInterface
      */
     public function gateway($gateway)
     {
-        return new static($this->config, $gateway);
-    }
-
-    /**
-     * Request payment
-     *
-     * @param Order $order
-     * @param $callback
-     * @return mixed
-     */
-    public function request(Order $order, $callback)
-    {
-        return $this->gateway->request($order, $callback);
-    }
-
-    /**
-     * Verify transaction
-     *
-     * @param $transactionId
-     * @return bool
-     */
-    public function verify($transactionId)
-    {
-        return $this->gateway->verify($transactionId);
-    }
-
-    /**
-     * Check if gateway supports currency
-     *
-     * @param string $currency
-     * @return bool|mixed
-     */
-    public function supportsCurrency(string $currency)
-    {
-        return $this->gateway->supportsCurrency($currency);
+        $config = Arr::get($this->config, "gateways.$gateway", []);
+        $driver = Arr::pull($config, 'driver');
+        return new $driver($config);
     }
 }
