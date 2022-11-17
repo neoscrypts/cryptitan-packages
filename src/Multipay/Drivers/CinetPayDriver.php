7b11591b156ce73221f0ce218d54f0a4f3e03127
oluwatosin-me <henryogunrinde@yahoo.com>
2022-11-17 14:43:35 +0100

    minor changes

--- src/Multipay/Drivers/CinetPayDriver.php
+++ src/Multipay/Drivers/CinetPayDriver.php
@@ -49,7 +49,10 @@ class CinetPayDriver extends AbstractDriver
     {
         $response = $this->client->post('payment', $this->buildRequest($order))->throw()->collect('data');
 
-        return $callback($response->get('payment_token'), $response->get('payment_url'));
+        return $callback(
+            $response->get('payment_token'),
+            $response->get('payment_url')
+        );
     }
 
     /**
@@ -85,18 +88,18 @@ class CinetPayDriver extends AbstractDriver
     {
         $amount = $order->getTotalAmount();
 
-        if ($amount->getValue() % 5 !== 0) {
+        if (fmod($amount->getValue(), 5)) {
             App::abort(422, 'Amount must be a multiple of 5');
         }
 
         return [
-            'amount'         => $amount->getValue(),
             'currency'       => $order->getCurrency()->getCurrency(),
+            'amount'         => $amount->getValue(),
             'description'    => "Order #{$order->getUuid()}",
             'transaction_id' => $order->getUuid(),
+            'site_id'        => $this->config('site_id'),
             'return_url'     => $this->callbackUrl($order),
             'apikey'         => $this->config('apikey'),
-            'site_id'        => $this->config('site_id')
         ];
     }
 }
